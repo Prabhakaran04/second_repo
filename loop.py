26778e3235e23a7a300bbbbf294aef20a3d90092
Prabhakaran <prabhakaran.t@osplabs.com>
2023-08-17 13:42:42 +0530

    this code is about loop

---
+++ loop.py
@@ -0,0 +1,17 @@
+# # for loop
+num1 = int(input('Enter a number'))
+for i in range(1, 11):
+    print(num1, '*', i, '=', num1*i)
+print('Table generated')
+
+# # while lopp
+# num1=int(input('Enter a number'))
+# while num1<=10:
+#     print(num1)
+#     num1+=1
+
+# # nested loop
+# num1=['name','age',['gender','job'],'salary']
+# for i in num1:
+#     for j in i:
+#         print(j)
